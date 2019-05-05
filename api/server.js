294af2ba5bcd030e81c011ffdb39915c95309745
renan <renansoares.cro-rj.org.br>
2019-05-05 13:42:44 -0300

    lógica para inclusao e atualizacao de pontos funcionando

--- api/server.js
+++ api/server.js
@@ -32,97 +32,86 @@ app.get("/", (req, res) => {
 
 
 app.post("/api", function (req, res) {
-
+    
     res.setHeader("Access-Control-Allow-Origin", '*')
     const dados = req.body;
-
+    
     console.log(dados.dataPonto)
-    retornaDataSeExistirNoBanco(dados);
     // res.send(dados);
     
     //testes dos dados do body
     //TODO
 
-    // db.open(function (err, mongoclient) {
-    //     mongoclient.collection("pontos", function (err, collection) {
-    //         //importante saber que o primeiro parametro se refere à chave do banco de dados e o segundo parametro com o objeto pesquisado
-    //         collection.find({ "idUsuario": { $eq: dados.idUsuario } }).toArray(function (err, results) {
-    //             if (results.length > 0) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection("pontos", function (err, collection) {
+            //importante saber que o primeiro parametro se refere à chave do banco de dados e o segundo parametro com o objeto pesquisado
+            collection.find({ "idUsuario": { $eq: dados.idUsuario },  "dataPonto": { $eq: dados.dataPonto } } ).toArray(function (err, results) {
+                if (results.length > 0) {
 
-    //                 //retornaDataSeExistirNoBanco();
-    //                 //atualizo o documento
+                    //atualizo o documento
                     
-    //                 collection.update(
-    //                     {idUsuario:dados.idUsuario},
-    //                     { $push: {"pontoRestoDoDia":dados.ponto}
-    //                     } ,
-    //                     {},
+                    collection.update(
+                        {idUsuario:dados.idUsuario,dataPonto:dados.dataPonto},
+                        { $push: {"pontoRestoDoDia":dados.ponto}
+                        } ,
+                        {},
 
-    //                 function (err, records) {
-    //                     if (err) {
-    //                         res.json({ msg: "Ocorreu um erro!" })
-    //                         console.log(err)
-    //                     } else {
-    //                         res.status(200).send("atualizar");
-    //                         //res.json({ msg: "Ponto registrado com sucesso!" })
-    //                     }
-    //                     mongoclient.close();
-    //                 })
+                    function (err, records) {
+                        if (err) {
+                            res.json({ msg: "Ocorreu um erro!" })
+                            console.log(err)
+                        } else {
+                            res.status(200).send("atualizar");
+                            //res.json({ msg: "Ponto registrado com sucesso!" })
+                        }
+                        mongoclient.close();
+                    })
 
-    //                 console.log("tem que atualizar!")
-    //                 mongoclient.close();
-    //             } else {
-    //                 //crio um novo documento
-    //                 collection.insert(dados, function (err, records) {
-    //                     if (err) {
-    //                         res.json({ msg: "Ocorreu um erro!" })
-    //                     } else {
-    //                         res.status(200).send("adicionar");
-    //                         //res.json({ msg: "Ponto registrado com sucesso!" })
-    //                     }
-    //                     mongoclient.close();
-    //                 })
-
-    //                 mongoclient.close();
-
-
-    //                 console.log("adicionou")
-    //             }
-
-    //         })
-    //     })
-    // })
+                    console.log("tem que atualizar!")
+                    mongoclient.close();
+                } else {
+                  //  crio um novo documento
+                    collection.insert(dados, function (err, records) {
+                        if (err) {
+                            res.json({ msg: "Ocorreu um erro!" })
+                        } else {
+                            res.status(200).send("adicionar");
+                            //res.json({ msg: "Ponto registrado com sucesso!" })
+                        }
+                        mongoclient.close();
+                    })
+                    console.log("tem que INSERIR")
+                    mongoclient.close();
 
 
-    
+                    console.log("adicionou")
+                }
 
-    // db.open(function(err, mongoclient){
-    //     mongoclient.collection("pontos", function(err,collection){
-    //         collection.insert(dados, function(err,records){
-    //             if(err){
-    //                 res.json({msg: "Ocorreu um erro!"})
-    //             }else{
-    //                 res.json({msg: "Ponto registrado com sucesso!"})
-    //             }
-    //             mongoclient.close();
-    //         })
-    //     })
-    // })    
+            })
+        })
+    })
+
 
 })
 
-function retornaDataSeExistirNoBanco(dados){
+function retornaDataAtualSeExistirNoBanco(dados){
 
     db.open(function (err, mongoclient) {
         mongoclient.collection("pontos", function (err, collection) {
             //importante saber que o primeiro parametro se refere à chave do banco de dados e o segundo parametro com o objeto pesquisado
             collection.find({ "dataPonto": { $eq: dados.dataPonto } }).toArray(function (err, results) {
-               
+                
+                var dataJaExiste = 0;
                 console.log(results)
-                if(results.length>0)// const data =  "data é igual a de hoje"
+                if(results.length>0){// const data =  "data é igual a de hoje"
                     console.log("data é igual a de hoje")
+                    mongoclient.close();
+                    return dataJaExiste = 1; 
+                }
                 else
                     console.log("data nao é igual a de hoje")
+                    mongoclient.close();
+                    return dataJaExiste;
             })
         })
     })
